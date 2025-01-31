0d45d68db71da9cd1bc8c0c875be9025bf82b342
MyMac <mymac@MyMacs-MacBook.lan>
2025-01-31 14:04:42 +0000

    Created app and initial authentication

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 require('dotenv/config')
 
+app.use(bodyParser.json())
+
 const filmsRoute = require('./routes/films')
 const authRoute = require('./routes/auth')
 
 app.use('/api/film',filmsRoute)
-app.use('./api/user', authRoute)
+app.use('/api/user',authRoute)
 
 
 mongoose.connect(process.env.DB_CONNECTOR)
